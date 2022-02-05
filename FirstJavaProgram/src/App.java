644949521de4a575daa6a8dbb28e270c8807b662
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-06 01:54:35 +0900

    [Study] #1 - 입출력문 연습, 기본 패키지 생성 및 컴파일
    입출력문 연습, 기본 패키지 생성 및 컴파일

--- FirstJavaProgram/src/App.java
+++ FirstJavaProgram/src/App.java
@@ -4,5 +4,10 @@ class FirstJavaProgram
     {
         System.out.println("welcome to Java");
         System.out.println("First Java program");
+        
+        // 아래는 출력문에 대한 연습
+        System.out.println(3.15)
+        System.out.println("3+5=" + 8)
+        System.out.println(3+5)
     }
 }
