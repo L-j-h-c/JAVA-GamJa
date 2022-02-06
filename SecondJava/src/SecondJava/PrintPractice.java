10afe8e030fd702edcd350d3df929f5dd71ac7cd
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-06 13:03:44 +0900

    [Study] #1 - SecondJAVA 클래스 생성
    SecondJAVA 클래스 생성

---
+++ SecondJava/src/SecondJava/PrintPractice.java
@@ -0,0 +1,15 @@
+package SecondJava;
+
+public class PrintPractice {
+	    public static void main(String[] args)
+	    {
+	        System.out.println("welcome to Java");
+	        System.out.println("First Java program");
+	        
+	        // 아래는 출력문에 대한 연습
+	        System.out.println(3.15);
+	        System.out.println("3+5=" + 8);
+	        System.out.println(3+5);
+	        System.out.println();
+	    }
+}
