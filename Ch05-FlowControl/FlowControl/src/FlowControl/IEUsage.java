1ba1e56a8ca809edfdc51f019753a8a29fa90287
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-11 23:34:27 +0900

    [Study] #3 - Ifelse 기본 예제와 생략
    Ifelse 기본 예제와 생략

--- Ch05-FlowControl/FlowControl/src/FlowControl/IEUsage.java
+++ Ch05-FlowControl/FlowControl/src/FlowControl/IEUsage.java
@@ -3,8 +3,50 @@ package FlowControl;
 public class IEUsage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num = 120;
+		
+		if(num<0)
+		{
+			System.out.println("0 미만");
+		}
+		else
+		{
+			if(num<100)
+			{
+				System.out.println("0이상 100 미만");
+			}
+			else
+			{
+				System.out.println("100 이상");
+			}
+		}
+		
+		// 하나의 문장으로 이루어진 선언문 중괄호 생
+		if(num<0)
+			System.out.println("0 미만");
+		else
+		{
+			if(num<100)
+				System.out.println("0이상 100 미만");
+			else
+				System.out.println("100 이상");
+		}
+		
+		// if else도 하나의 문장이기에 중괄호 생
+		if(num<0)
+			System.out.println("0 미만");
+		else
+			if(num<100)
+				System.out.println("0이상 100 미만");
+			else
+				System.out.println("100 이상");
+		
+		if(num<0)
+			System.out.println("0 미만");
+		else if(num<100)
+				System.out.println("0이상 100 미만");
+		else
+				System.out.println("100 이상");
 	}
 
 }
